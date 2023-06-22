e3e4e42b085ea114d5c97bbf590318a3b646a6a3
Malith Anjana <malithanjana97@gmail.com>
2023-06-22 18:17:47 +0530

    Initial commit

---
+++ reminders-app/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <button className='btn btn-lg btn-primary'>Click ME</button>
+        
+    </>
+  )
+}
+
+export default App
